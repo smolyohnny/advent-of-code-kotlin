140a1feda056bd51c5e0a722a8f27dd33df365a6
yohnny <114059682+smolyohnny@users.noreply.github.com>
2024-12-02 19:05:43 +0100

    2024 Day2

---
+++ src/y2024/Day2.kt
@@ -0,0 +1,67 @@
+package y2024
+
+import java.nio.file.Files
+import kotlin.io.path.Path
+
+private val input: MutableList<String> = Files.readAllLines(Path("src\\y2024\\inputs\\day2.txt"))
+private var sum1 = 0
+private var sum2 = 0
+
+fun main() {
+    solvePart1()
+    solvePart2()
+}
+
+private fun solvePart1() {
+    for (line in input) {
+        val strArr = line.split(" ").toTypedArray()
+        val intList: MutableList<Int> = mutableListOf()
+
+        for (i in strArr) intList.add(i.toInt())
+
+        var IsSafe = false
+        for (i in 0..intList.size - 2) {
+            if (intList[i] > intList[i + 1] && intList[i] - intList[i + 1] <= 3) {
+                if (i == intList.size - 2) IsSafe = true
+            } else break
+        }
+        for (i in 0..intList.size - 2) {
+            if (intList[i] < intList[i + 1] && intList[i + 1] - intList[i] <= 3) {
+                if (i == intList.size - 2) IsSafe = true
+            } else break
+        }
+        if (IsSafe) sum1++
+    }
+    println(sum1)
+}
+
+private fun solvePart2() {
+    for (line in input) {
+        var IsSafe = false
+        val strArr = line.split(" ").toTypedArray()
+        val intList: MutableList<Int> = mutableListOf()
+        for (i in strArr) intList.add(i.toInt())
+        for (l in 0..<intList.size) {
+            val altList = removeElement(intList, l)
+            for (i in 0..altList.size - 2) {
+                if (altList[i] > altList[i + 1] && altList[i] - altList[i + 1] <= 3) {
+                    if (i == altList.size - 2) IsSafe = true
+                } else break
+            }
+            for (i in 0..altList.size - 2) {
+                if (altList[i] < altList[i + 1] && altList[i + 1] - altList[i] <= 3) {
+                    if (i == altList.size - 2) IsSafe = true
+                } else break
+            }
+        }
+        if (IsSafe) sum2++
+    }
+    println(sum2)
+}
+
+private fun removeElement(list: MutableList<Int>, index: Int): MutableList<Int> {
+    val tempList: MutableList<Int> = mutableListOf()
+    for (i in list) tempList.add(i)
+    tempList.removeAt(index)
+    return tempList
+}
